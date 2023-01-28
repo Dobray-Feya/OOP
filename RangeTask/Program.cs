9c56e664efcb670f8720bb77956055ccd349fcae
Polina Arkashova <arkashova_p@mail.ru>
2023-01-29 00:32:48 +0700

    Copied solutions of task from C# course for beginners

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -1,11 +1,37 @@
-﻿namespace RangeTask
+﻿namespace Range
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, my World!");
-            Console.WriteLine("Hello, dear World!");
+            double x11 = 300;
+            double x12 = -100;
+
+            double x21 = 300;
+            double x22 = 200;
+
+            Range range1 = new Range(x11, x12);
+            Range range2 = new Range(x21, x22);
+
+            Console.WriteLine($"Длина отрезка 1 равна {range1.GetLength()}.");
+            Console.WriteLine($"Длина отрезка 2 равна {range2.GetLength()}.");
+
+            if (range1.IsInside(range2.From) && range1.IsInside(range2.To))
+            {
+                Console.WriteLine("Отрезок 2 лежит внутри отрезка 1.");
+            }
+            else if (range2.IsInside(range1.From) && range2.IsInside(range1.To))
+            {
+                Console.WriteLine("Отрезок 1 лежит внутри отрезка 2.");
+            }
+            else if (range2.IsInside(range1.From) || range2.IsInside(range1.To))
+            {
+                Console.WriteLine("Отрезки пересекаются");
+            }
+            else
+            {
+                Console.WriteLine("Отрезки не пересекаются");
+            }
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RangeTask
+﻿namespace Range
 {
     internal class Range
     {
-        public double from;
-        public int to;
+        // это автосвойства объекта
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        // это конструктор для инициализации объекта
+        public Range(double from, double to)
+        {
+            if (from < to)
+            {
+                From = from;
+                To = to;
+            }
+            else
+            {
+                From = to;
+                To = from;
+            }
+        }
+
+        // это методы объекта
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return From <= number && number <= To;
+        }
     }
 }
