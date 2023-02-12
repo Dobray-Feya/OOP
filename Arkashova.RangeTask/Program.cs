a57f55636a553800ee15699dc26673bfbd4cb401
Polina Arkashova <arkashova_p@mail.ru>
2023-02-12 22:40:10 +0700

    Change directory name

---
+++ Arkashova.RangeTask/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Arkashova.RangeTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = -5.0;
+            double x2 = 0.0;
+            double x3 = 3.0;
+            double x4 = 5.0;
+            double x5 = 10.0;
+
+            Range[] ranges =
+            {
+                new Range(x1, x2),
+                new Range(x2, x3),
+                new Range(x3, x4),
+                new Range(x2, x4),
+                new Range(x1, x4),
+                new Range(x2, x5)
+            };
+
+            // Часть 1
+
+            Console.WriteLine("Длина отрезка:");
+            foreach (Range item in ranges)
+            {
+                Console.WriteLine($"[{item.From}, {item.To}] равна {item.GetLength()}");
+            }
+
+            Console.WriteLine();
+
+            double point = 1.5;
+            Console.WriteLine($"Принадлежит точка {point} отрезку?");
+
+            foreach (Range item in ranges)
+            {
+                Console.WriteLine($"[{item.From}, {item.To}] — {item.IsInside(point)}");
+            }
+
+            Console.WriteLine();
+
+            // Часть 2
+
+            Range testRange = new Range(-1, 1);
+
+            Console.WriteLine($"Пересечение отрезка [{testRange.From}, {testRange.To}] с отрезками:");
+
+            foreach (Range item in ranges)
+            {
+                Console.Write($"[{item.From}, {item.To}] — ");
+
+                Range? intersection = item.GetIntersection(testRange);
+
+                if (intersection == null)
+                {
+                    Console.WriteLine("пусто");
+                }
+                else
+                {
+                    Console.WriteLine($"[{intersection.From}, {intersection.To}]");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Объединение отрезка [{testRange.From}, {testRange.To}] с отрезками:");
+
+            foreach (Range item in ranges)
+            {
+                Console.Write($"[{item.From}, {item.To}] — ");
+
+                Range[] union = item.GetUnion(testRange);
+
+                foreach (Range unionPiece in union)
+                {
+                    Console.Write($"[{unionPiece.From}, {unionPiece.To}] ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Результат вычитания отрезка [{testRange.From}, {testRange.To}] из отрезков:");
+
+            foreach (Range item in ranges)
+            {
+                Console.Write($"[{item.From}, {item.To}] — ");
+
+                Range[] difference = item.GetDifference(testRange);
+
+                if (difference.Length == 0)
+                {
+                    Console.WriteLine("пусто");
+                }
+                else
+                {
+                    foreach (Range differencePiece in difference)
+                    {
+                        Console.Write($"[{differencePiece.From}, {differencePiece.To}] ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Arkashova.RangeTask/Range.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Arkashova.RangeTask
+{
+    internal class Range
+    {
+        // это автосвойства объекта
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        // это конструктор для инициализации объекта
+        public Range(double from, double to)
+        {
+            if (from < to)
+            {
+                From = from;
+                To = to;
+            }
+            else
+            {
+                From = to;
+                To = from;
+            }
+        }
+
+        // это методы объекта
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return From <= number && number <= To;
+        }
+
+        public Range? GetIntersection(Range range)
+        {
+            if ((range.To <= From) || (range.From >= To))
+            {
+                return null;
+            }
+
+            return new Range(Math.Max(From, range.From), Math.Min(To, range.To));
+        }
+
+        public Range[] GetUnion(Range range)
+        {
+            if (range.To < From)
+            {
+                return new Range[] { new Range(range.From, range.To), new Range(From, To) };
+            }
+
+            if (range.From > To)
+            {
+                return new Range[] { new Range(From, To), new Range(range.From, range.To) };
+            }
+
+            return new Range[] { new Range(Math.Min(From, range.From), Math.Max(To, range.To)) };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            if ((range.From <= From) && (range.To >= To))
+            {
+                return Array.Empty<Range>();  // это пустой массив
+            }
+
+            if ((range.To <= From) || (range.From >= To))
+            {
+                return new Range[] { new Range(From, To) };
+            }
+
+            if ((range.From > From) && (range.To < To))
+            {
+                return new Range[] { new Range(From, range.From), new Range(range.To, To) };
+            }
+
+            if ((range.From > From) && (range.From < To) )
+            {
+                return new Range[] { new Range(From, range.From) };
+            }
+
+            return new Range[] { new Range(range.To, To) };
+        }
+    }
+}
