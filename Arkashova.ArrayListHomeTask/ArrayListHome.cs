452eeb68c0bced9774bcd9d32a5e52c7370c0764
Polina Arkashova <arkashova_p@mail.ru>
2023-03-05 08:38:58 +0700

    Fixed teacher's comments (2 iteration).

--- Arkashova.ArrayListHomeTask/ArrayListHome.cs
+++ Arkashova.ArrayListHomeTask/ArrayListHome.cs
@@ -29,48 +29,60 @@
                 Console.WriteLine($"Вместимость списка: {linesList.Capacity}");
                 Console.WriteLine($"Количество элементов в списке: {linesList.Count}");
             }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Файл не найден.");
+                Console.WriteLine("Описание ошибки:");
+                Console.WriteLine(e.Message);
+            }
             catch (Exception e)
             {
-                Console.WriteLine($"Не удалось записать строки файла в список.");
+                Console.WriteLine("Не удалось записать строки файла в список.");
                 Console.WriteLine("Полное описание ошибки:");
-                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
             }
 
             Console.WriteLine();
             Console.WriteLine("Задача 2. Есть список из целых чисел. Удалить из него все четные числа. В этой задаче новый список создавать нельзя.");
             Console.WriteLine();
 
-            List<int> numbersList = new List<int> { 0, 1, 2, 3, 4 };
-            Console.WriteLine("Исходный список: " + String.Join<int>(" ", numbersList));
+            List<int> numbersList = new List<int> { 0, 2, 2, 2, 4, 1, 4 };
+            Console.WriteLine("Исходный список: " + string.Join<int>(" ", numbersList));
 
-            for (int i = 0; i < numbersList.Count; i++)
+            int index = 0;
+
+            while (index < numbersList.Count)
             {
-                if (numbersList[i] % 2 == 0)
+                if (numbersList[index] % 2 == 0)
                 {
-                    numbersList.Remove(numbersList[i]);
+                    numbersList.RemoveAt(index);
+                }
+                else
+                {
+                    index++;
                 }
             }
 
-            Console.WriteLine("Список после удаления четных элементов: " + String.Join<int>(" ", numbersList));
-
+            Console.WriteLine("Список после удаления четных элементов: " + string.Join<int>(" ", numbersList));
             Console.WriteLine();
+
             Console.WriteLine("Задача 3. Есть список из целых чисел. Надо создать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений.");
             Console.WriteLine();
 
-            List<int> sourceNumbersList = new List<int> { 0, 1, 0, 2, 0, 1, 3, 2, 4, 5, 5, 0, };
-            Console.WriteLine("Исходный список: " + String.Join<int>(" ", sourceNumbersList));
+            List<int> sourceNumbersList = new List<int> { 0, 1, 0, 2, 0, 1, 3, 2, 4, 5, 5, 0 };
+            Console.WriteLine("Исходный список: " + string.Join<int>(" ", sourceNumbersList));
 
-            List<int> withoutDuplicationsNumbersList = new List<int> { sourceNumbersList[0] };
+            List<int> numbersListWithoutDuplications = new List<int>(sourceNumbersList.Count);
 
             foreach (int number in sourceNumbersList)
             {
-                if (!withoutDuplicationsNumbersList.Contains(number))
+                if (!numbersListWithoutDuplications.Contains(number))
                 {
-                    withoutDuplicationsNumbersList.Add(number);
+                    numbersListWithoutDuplications.Add(number);
                 }
             }
 
-            Console.WriteLine("Список Без повторяющихся элементов: " + String.Join<int>(" ", withoutDuplicationsNumbersList));
+            Console.WriteLine("Список Без повторяющихся элементов: " + string.Join<int>(" ", numbersListWithoutDuplications));
         }
     }
 }
