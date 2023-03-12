17fd8705ff76ffe323ad4345fb71f2f11a0070c0
Polina Arkashova <arkashova_p@mail.ru>
2023-03-12 08:15:57 +0700

    ArrayListTask. First commit.

---
+++ Arkashova.ArayListTask/ArrayList.cs
@@ -0,0 +1,306 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Arkashova.ArayListTask
+{
+    public class ArrayList<T> : IList<T>
+    {
+        private int count;
+
+        private const int defaultCapacity = 4;
+
+        private T[] items;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        /// <summary>
+        ///     Gets or sets the total number of elements the internal data structure can hold without resizing.
+        /// </summary>
+        /// <returns>
+        ///     Returns the number of elements that the list can contain before resizing is required.
+        /// </returns>
+        /// <exception>ArgumentOutOfRangeException</exception>
+        public int Capacity
+        {
+            get
+            {
+                return items.Length;
+            }
+
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Нельзя сделать вместимость списка равной 0.");
+                }
+
+                if (value < Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Нельзя сделать вместимость списка равной {value}, " +
+                                                                         $"т.к. это меньше, чем текущее количество элементов в списке ({Count})");
+                }
+
+                Array.Resize(ref items, value);
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException(); // Пока не знаю, как реализовать
+
+        public ArrayList()
+        {
+            count = 0;
+
+            items = new T[defaultCapacity];
+        }
+
+        public ArrayList(int capacity)
+        {
+            count = 0;
+
+            items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+
+            get
+            {
+                if (count == 0)
+                {
+                    throw new ArgumentException("Нельзя получить элемент списка. Список не содержит элементов.");
+                }
+
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException($" Нельзя получить элемент списка по индексу {index}. " +
+                                                       $" Индекс элемента должен быть от 0 до {count - 1}.");
+                }
+
+                return items[index];
+            }
+
+            set
+            {
+                if (count == 0)
+                {
+                    throw new ArgumentException("Нельзя задать значение элементу списка. Список не содержит элементов.");
+                }
+
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException($" Нельзя задать элемент списка по индексу {index}. " +
+                                                       $"Индекс элемента должен быть от 0 до {count - 1}.");
+                }
+
+                items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (count >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[count] = item;
+
+            count++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            Array.Resize(ref items, items.Length * 2);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException($"Нельзя удалить элемент списка по индексу {index}. " +
+                                                   $"Индекс элемента должен быть от 0 до {count - 1}.");
+            }
+
+            if (index < count - 1)
+            {
+                Array.Copy(items, index + 1, items, index, count - index - 1);
+            }
+
+#pragma warning disable CS8601 // Possible null reference assignment. // Пришлось отключить warning (и ниже в одном месте)
+            items[count - 1] = default;  // Заметка для себя: default для ссылочных типов выдает null, а для value - типов – экземпляр структуры, созданный через конструктор без аргументов
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            count--;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = 0;
+
+            while (index < count)
+            {
+                if (items[index] is null && item is null)
+                {
+                    RemoveAt(index);
+
+                    return true;
+                }
+
+                if (items[index] is not null && items[index]!.Equals(item)) // Пришлось поставить !, чтобы не было warning'а (и ниже в трех подобных местах)
+                {
+                    RemoveAt(index);
+
+                    return true;
+                }
+
+                index++;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+
+            for (int i = 0; i < Count; i++)
+            {
+#pragma warning disable CS8601 // Possible null reference assignment.
+                items[i] = default;
+#pragma warning restore CS8601 // Possible null reference assignment.
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (Capacity == count)
+            {
+                IncreaseCapacity();
+            }
+
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException($"Нельзя вставить элемент списка по индексу {index}. " +
+                                                   $"Индекс элемента должен быть от 0 до {count}.");
+            }
+
+            if (index < count)
+            {
+                Array.Copy(items, index, items, index + 1, count - index + 1);
+            }
+
+            items[index] = item;
+
+            count++;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), "Вставка списка в массив невозможна. Целевой массив равен null.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Вставка списка в массив невозможна. Целевой массив пуст.",
+                                           $"{nameof(array.Length)}");
+            }
+
+            if (arrayIndex + count > array.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(array.Length)}, {nameof(Count)}, {nameof(arrayIndex)}",
+                                                      $"Превышен размер маcсива {array.Length}. Список длины {count} не может быть вставлен в массив по индексу {arrayIndex}.");
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i + arrayIndex] = items[i];
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] is null && item is null)
+                {
+                    return true;
+                }
+
+                if (items[i] is not null && items[i]!.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] is null && item is null)
+                {
+                    return i;
+                }
+
+                if (items[i] is not null && items[i]!.Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            if (count == 0)
+            {
+                return "";
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                stringBuilder.Append(this[i])
+                             .Append(", ");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Changes array capacity to elements in list count, if this count is less than 90 % of array capacity.
+        /// </summary>
+        public void TrimExcess()
+        {
+            if (count < 0.9 * Capacity)
+            {
+                Capacity = count;
+            };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
---
+++ Arkashova.ArayListTask/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Arkashova.ArayListTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList<int> integersList = new ArrayList<int> { 1, 2, 1, 3, 3, 4, 5 };
+
+            Console.WriteLine("Исходный список: " + integersList);
+            Console.WriteLine();
+
+            integersList.RemoveAt(1);
+            Console.WriteLine("RemoveAt(1) -> " + integersList);
+            Console.WriteLine();
+
+            Console.WriteLine("IndexOf(1) = " + integersList.IndexOf(1));
+            Console.WriteLine("IndexOf(2) = " + integersList.IndexOf(2));
+            Console.WriteLine();
+
+            Console.WriteLine(integersList);
+            Console.WriteLine("Remove(1) = " + integersList.Remove(1) + " -> " + integersList);
+            Console.WriteLine("Remove(2) = " + integersList.Remove(2) + " -> " + integersList);
+            Console.WriteLine("Remove(3) = " + integersList.Remove(3) + " -> " + integersList);
+            Console.WriteLine("Remove(3) = " + integersList.Remove(3) + " -> " + integersList);
+            Console.WriteLine();
+
+            Console.WriteLine(integersList);
+            int index1 = 2;
+            integersList.Insert(index1, 100);
+            Console.WriteLine($"Insert({index1}, 100) -> " + integersList);
+            Console.WriteLine();
+
+            int index2 = 6;
+            Console.WriteLine($"Вставить список {integersList} в масссив длины 10, начиная с позиции {index2}:");
+            int[] array = new int[10];
+            integersList.CopyTo(array, index2);
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine();
+
+            integersList.Add(101);
+            integersList.Add(102);
+            integersList.Add(103);
+            integersList.Add(104);
+            integersList.Add(105);
+
+            Console.WriteLine($"{integersList}: Count = {integersList.Count}, Capacity = {integersList.Capacity}");
+            integersList.Capacity = 10;
+            Console.WriteLine("Capacity = 10");
+            integersList.TrimExcess();
+            Console.WriteLine($"TrimExcess() -> Count = {integersList.Count}, Capacity = {integersList.Capacity}");
+
+            integersList.Capacity = 11;
+            Console.WriteLine("Capacity = 11");
+            integersList.TrimExcess();
+            Console.WriteLine($"TrimExcess() -> Count = {integersList.Count}, Capacity = {integersList.Capacity}");
+            Console.WriteLine();
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            ArrayList<string> linesList = new ArrayList<string> { null, "hello", "hello", "1234", "" };
+            Console.WriteLine(linesList);
+            Console.WriteLine("IndexOf(null) = " + linesList.IndexOf(null));
+            Console.WriteLine("Contains(null) -> " + linesList.Contains(null));
+            Console.WriteLine("Remove(null) = " + linesList.Remove(null) + " -> " + linesList);
+            Console.WriteLine("Remove(\"\") = " + linesList.Remove(string.Empty) + " -> " + linesList);
+
+            linesList.Add(null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            Console.WriteLine("Add(null) -> " + linesList);
+            linesList.Add("5678");
+            Console.WriteLine("Add(5678) -> " + linesList);
+            Console.WriteLine();
+
+            Console.WriteLine("Проход по списку с помощью foreach:");
+
+            foreach (string line in linesList)
+            {
+                Console.WriteLine(line);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Проход по списку с помощью итератора:");
+
+            IEnumerator<string> iterator = linesList.GetEnumerator();
+
+            while (iterator.MoveNext())
+            {
+                string text = iterator.Current;
+
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
