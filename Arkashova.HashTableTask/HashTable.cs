36c999bb54e0db9b256beab6d7cc05e4b7b06a3e
Polina Arkashova <arkashova_p@mail.ru>
2023-03-25 08:04:33 +0700

    HashTableTask. First commit.

---
+++ Arkashova.HashTableTask/HashTable.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Text;
+using Arkashova.ArayListTask;
+
+namespace Arkashova.HashTableTask
+{
+    public class HashTable<T> : ICollection<T>
+    {
+        private ArrayList<T>[] lists; //Заметка для себя: здесь можно любой список, например, стандартный List, но я взяла свой ArrayList 
+
+        public int Count { get; private set; }
+
+        int modCount;
+
+        public HashTable(int size)
+        {
+            lists = new ArrayList<T>[size];
+        }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            int index = GetItemIndex(item);
+
+            if (lists[index] is null)
+            {
+                lists[index] = new ArrayList<T>();
+            }
+
+            lists[index].Add(item);
+
+            Count++;
+            modCount++;
+        }
+
+        // Считаю, что в хэш-таблице могут быть элементы null и для них зарезервирован 0-й элемент массива.
+        private int GetItemIndex(T item)
+        {
+            return item is null ? 0 : 1 + Math.Abs(item.GetHashCode() % (lists.Length - 1));
+        }
+
+        public bool Remove(T item)
+        {
+            bool IsRemoved = lists[GetItemIndex(item)].Remove(item);
+
+            if (IsRemoved)
+            {
+                Count--;
+                modCount++;
+            }
+
+            return IsRemoved;
+        }
+
+        public void Clear()
+        {
+            if (lists.Length == 0)
+            {
+                return;
+            }
+
+            Array.Clear(lists, 0, lists.Length);
+
+            Count = 0;
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            return lists[GetItemIndex(item)].Contains(item);
+        }
+
+        public void CopyTo(T?[] array, int arrayIndex)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array), "Вставка хэш-таблицы в массив невозможна. Целевой массив равен null.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(array.Length)}, {nameof(Count)}, {nameof(arrayIndex)}",
+                                                      "Вставка хэш-таблицы в массив невозможна. Целевой массив пуст.");
+            }
+
+            if (arrayIndex + Count > array.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(array.Length)}, {nameof(Count)}, {nameof(arrayIndex)}",
+                                                      $"Превышен размер маcсива {array.Length}. Хэш-таблица размера {Count} " +
+                                                      $"не может быть вставлена в массив по индексу {arrayIndex}.");
+            }
+
+            int insertedItemsCount = arrayIndex;
+
+            foreach (ArrayList<T> list in lists)
+            {
+                if (list is not null)
+                {
+                    list.CopyTo(array, insertedItemsCount);
+
+                    insertedItemsCount += list.Count;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Count == 0)
+            {
+                return "[]";
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                stringBuilder.Append("[");
+
+                if (lists[i] is not null)
+                {
+                    for (int j = 0; j < lists[i].Count; j++)
+                    {
+                        if (lists[i][j] is null)
+                        {
+                            stringBuilder.Append("null");
+                        }
+                        else
+                        {
+                            stringBuilder.Append(lists[i][j]!.ToString());
+                        }
+
+                        stringBuilder.Append("; ");
+                    }
+
+                    stringBuilder.Remove(stringBuilder.Length - 2, 2);
+                }
+
+                stringBuilder.Append("]\n\r");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int initialModCount = modCount;
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                ArrayList<T> currentList = lists[i];
+
+                if (currentList is not null)
+                {
+                    for (int j = 0; j < currentList.Count; j++)
+                    {
+                        if (modCount != initialModCount)
+                        {
+                            throw new InvalidOperationException("Проход итератором по хэш-таблице невозможен, потому что с момента начала обхода хэш-таблица была изменена.");
+                        }
+
+                        yield return currentList[j];
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Arkashova.HashTableTask/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace Arkashova.HashTableTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<double?> hashtable1 = new HashTable<double?>(15) { null, 1, 2, 1, null, 6, 2, 3, 4, 5, 6, 7, 9, 10, 5.11, 5.221, 5.301, 5.5, 5.666, 5.7, 5.88888, 5.9 };
+            Console.WriteLine(hashtable1);
+            Console.WriteLine($"Count = {hashtable1.Count}");
+            Console.WriteLine();
+
+            hashtable1.Add(null);
+            hashtable1.Add(111111);
+            Console.WriteLine("Add(null), Add(111111):");
+            Console.WriteLine();
+            Console.WriteLine(hashtable1);
+            Console.WriteLine($"Count = {hashtable1.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine("Remove(null) = " + hashtable1.Remove(null));
+            Console.WriteLine("Remove(1) = " + hashtable1.Remove(1));
+            Console.WriteLine("Remove(100) = " + hashtable1.Remove(100));
+            Console.WriteLine();
+            Console.WriteLine(hashtable1);
+            Console.WriteLine($"Count = {hashtable1.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine("Contains(1) = " + hashtable1.Contains(1));
+            Console.WriteLine("Contains(null) = " + hashtable1.Contains(null));
+            Console.WriteLine("Contains(-1) = " + hashtable1.Contains(-1));
+            Console.WriteLine();
+
+            HashTable<double> hashtable2 = new HashTable<double>(16) { 1, 2, 1, 6, 2, 3, 4, 5, 6, 7, 9, 10, 5.11, 5.221, 5.301, 5.5, 5.666, 5.7, 5.88888, 5.9 };
+            Console.WriteLine(hashtable2);
+            Console.WriteLine($"Count = {hashtable2.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine("Проход по хэш-таблице с помощью foreach:");
+
+            foreach (double item in hashtable2)
+            {
+                Console.Write(item + " | ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int index = 4;
+            const int arrayLength = 28;
+            double[] array = new double[arrayLength];
+            Console.WriteLine($"Вставить хэш-таблицу в масссив длины {arrayLength}, начиная с позиции {index}:");
+            hashtable2.CopyTo(array, index);
+            Console.WriteLine(string.Join("| ", array));
+            Console.WriteLine();
+
+            hashtable2.Clear();
+            Console.WriteLine("Clear():");
+            Console.WriteLine(hashtable2);
+            Console.WriteLine($"Count = {hashtable2.Count}");
+            Console.WriteLine();
+
+            HashTable<double?> hashtable3 = new HashTable<double?>(10) { null, 1, 2, 1, null, 6, 7, 8 };
+            Console.WriteLine(hashtable3);
+            Console.WriteLine($"Count = {hashtable3.Count}");
+            Console.WriteLine();
+            double?[] array3 = new double?[15];
+            Console.WriteLine($"Вставить хэш-таблицу в масссив длины 15, начиная с позиции 0:");
+            hashtable3.CopyTo(array3, 0);
+            Console.WriteLine(string.Join("| ", array3));
+            Console.WriteLine();
+
+            Console.WriteLine("Проход по хэш-таблице с помощью foreach:");
+
+            foreach (double? item in hashtable3)
+            {
+                Console.Write(item + " | ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Проход по хэш-таблице с помощью итератора:");
+
+            IEnumerator<double?> iterator = hashtable3.GetEnumerator();
+
+            try
+            {
+                int iteration = 0;
+
+                while (iterator.MoveNext())
+                {
+                    if (iteration == 5)
+                    {
+                        hashtable3.Remove(1);
+                    }
+
+                    iteration++;
+
+                    Console.WriteLine(iterator.Current);
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
