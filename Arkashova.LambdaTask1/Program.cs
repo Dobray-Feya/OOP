e1953b86c0c760a3366b4bd99e608bd84c2358b9
Polina Arkashova <arkashova_p@mail.ru>
2023-03-31 21:27:21 +0700

    LambdaTask1. Fixed teacher's comments (2 iteration).

--- Arkashova.LambdaTask1/Program.cs
+++ Arkashova.LambdaTask1/Program.cs
@@ -6,17 +6,17 @@
         {
             var persons = new List<Person>
             {
-                new Person("Саша", 30),
-                new Person("Петя", 22),
-                new Person("Петя", 18),
-                new Person("Саша", 19),
-                new Person("Лена", 20),
-                new Person("Маша", 44),
-                new Person("Нина", 45),
-                new Person("Маша", 22),
-                new Person("Саша", 0),
-                new Person("Таня", 22),
-                new Person("Лена", 1)
+                new("Саша", 30),
+                new("Петя", 22),
+                new("Петя", 18),
+                new("Саша", 19),
+                new("Лена", 20),
+                new("Маша", 4),
+                new("Нина", 45),
+                new("Маша", 22),
+                new("Саша", 40),
+                new("Таня", 22),
+                new("Лена", 1)
             };
 
             foreach (var person in persons)
@@ -28,36 +28,42 @@
 
             // А) получить список уникальных имен
 
-            var unicNames = persons
-                .Select(person => person.Name)
+            var uniqueNames = persons
+                .Select(p => p.Name)
                 .Distinct()
                 .ToList();
 
             // Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.
 
-            Console.WriteLine("Имена: " + string.Join(", ", unicNames));
+            Console.WriteLine("Имена: " + string.Join(", ", uniqueNames));
             Console.WriteLine();
 
             // В) получить список людей младше 18, посчитать для них средний возраст
 
-            var youngPersons = persons
-                .Where(person => person.Age < 18);
+            var youngPersons = persons.Where(p => p.Age < 18);
 
-            Console.WriteLine("Люди младше 18 лет:");
-            Console.WriteLine(string.Join(", ", youngPersons.Select(person => person.Name + " (" + person.Age +")")));
+            var youngPersonsNamesAndAges = youngPersons.Select(p => p.Name + " (" + p.Age + ")");
 
-            var youngPersonsAverageAge = youngPersons
-                .Select(person => person.Age)
-                .Average();
+            if (youngPersons.ToList().Count == 0)
+            {
+                Console.WriteLine("Людей младше 18 лет нет.");
+            }
+            else
+            {
+                Console.WriteLine("Люди младше 18 лет:");
+                Console.WriteLine(string.Join(", ", youngPersonsNamesAndAges));
 
-            Console.WriteLine("Средний возраст людей младше 18 лет = " + youngPersonsAverageAge);
+                var youngPersonsAverageAge = youngPersons.Average(p => p.Age);
+                Console.WriteLine("Средний возраст людей младше 18 лет = " + youngPersonsAverageAge);
+            }
+
             Console.WriteLine();
 
             // Г) при помощи группировки получить Dictionary, в котором ключи – имена, а значения – средний возраст
 
-            var personsAverageAgesByNames = persons.
-                GroupBy(person => person.Name, person => person.Age).
-                ToDictionary(name => name.Key, ages => ages.ToList().Average());
+            var personsAverageAgesByNames = persons
+                .GroupBy(p => p.Name, p => p.Age)
+                .ToDictionary(g => g.Key, g => g.Average());
 
             Console.WriteLine("Средний возраст людей для каждого имени:");
             Console.WriteLine(string.Join(", ", personsAverageAgesByNames));
@@ -66,9 +72,9 @@
             // Д) получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
 
             var personsFrom20To45Ages = persons
-                .Where(person => (person.Age >= 20) && (person.Age <= 45))
-                .OrderByDescending(person => person.Age)
-                .Select(person => person.Name)
+                .Where(p => (p.Age >= 20) && (p.Age <= 45))
+                .OrderByDescending(p => p.Age)
+                .Select(p => p.Name)
                 .ToList();
 
             Console.WriteLine("Имена людей в возрасте от 20 до 45 лет (в порядке убывания возраста):");
