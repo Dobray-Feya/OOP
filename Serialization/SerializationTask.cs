293cefd59f34ea5abc93196bd08d9782928ba93a
Polina Arkashova <arkashova_p@mail.ru>
2023-04-01 16:22:24 +0700

    Serialization task. First commit.

---
+++ Serialization/SerializationTask.cs
@@ -0,0 +1,42 @@
+﻿//https://professorweb.ru/my/csharp/thread_and_files/level4/4_6.php
+
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    internal class SerializationTask
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа сериализует и дисериализует симметричную матрицу.");
+            Console.WriteLine();
+            Console.WriteLine("Матрица:");
+
+            var matrixBeforeSerialization = new SymmetricMatrix(6);
+            matrixBeforeSerialization.Print();
+
+            var serializationFileName = "..\\..\\..\\data.txt";
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (Stream stream = new FileStream(serializationFileName, FileMode.Create, FileAccess.Write))
+            {
+#pragma warning disable SYSLIB0011  // Сериализация BinaryFormatter устарела - компиллятор выдает предупреждение.
+                formatter.Serialize(stream, matrixBeforeSerialization);
+
+                Console.WriteLine("Матрица успешно сериализована в файл " + Path.GetFullPath(serializationFileName));
+            }
+
+            using (Stream stream = new FileStream(serializationFileName, FileMode.Open, FileAccess.Read))
+            {
+                var matrixAfterSerialization = (SymmetricMatrix)formatter.Deserialize(stream);
+
+                Console.WriteLine($"Матрица успешно десериализована из файла {Path.GetFullPath(serializationFileName)}:");
+
+                matrixAfterSerialization.Print();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Serialization/SymmetricMatrix.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    internal class SymmetricMatrix //: ISerializable - здесь можно было бы реализовать интерфейс ISerializable, но это сложно
+    {
+        private int size;
+
+        public double[,] matrix;
+
+        public SymmetricMatrix(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException("Не удалось создать матрицу. Размер матрицы должен быть больше нуля.", nameof(size));
+            }
+
+            this.size = size;
+
+            matrix = new double[size, size];
+
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i, i] = random.Next(0, 101);
+
+                for (int j = i + 1; j < size; j++)
+                {
+                    matrix[i, j] = random.Next(0, 101);
+                    matrix[j, i] = matrix[i, j];
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"{matrix[i, j],5}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        [OnSerializing] // если спрятать под комментарием этот и следующий метод, то будет применена стандартная сериализация.
+        // У моей сериализации должен был получится файл меньшего размера, чем у стандартной, но получилось наоборот...НЕ знаю, как правильно.
+        internal void OnSerializingMethod(StreamingContext context)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        [OnDeserialized]
+        internal void OnDeserializedMethod(StreamingContext context)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    matrix[i, j] = matrix[j, i];
+                }
+            }
+        }
+    }
+}
